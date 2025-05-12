9005fa688a5be9a87275c95566b17da90b6072dd
Nazila Valiyeva (Nazilə Vəliyeva) <Nazilaveliyeva94@gmail.com>
2025-05-12 15:24:38 +0400

    Add project files.

---
+++ lesson4_ededler/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace lesson4_ededler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kvadratkub = new System.Windows.Forms.Button();
+            this.bolunen5 = new System.Windows.Forms.Button();
+            this.ReqemCemi = new System.Windows.Forms.Button();
+            this.ededtersi = new System.Windows.Forms.Button();
+            this.sade = new System.Windows.Forms.Button();
+            this.faktorial = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(509, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(22, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(354, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nəticə burada görünəcək";
+            // 
+            // kvadratkub
+            // 
+            this.kvadratkub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.kvadratkub.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kvadratkub.ForeColor = System.Drawing.Color.Blue;
+            this.kvadratkub.Location = new System.Drawing.Point(12, 208);
+            this.kvadratkub.Name = "kvadratkub";
+            this.kvadratkub.Size = new System.Drawing.Size(200, 44);
+            this.kvadratkub.TabIndex = 2;
+            this.kvadratkub.Text = "KvadratKub";
+            this.kvadratkub.UseVisualStyleBackColor = true;
+            // 
+            // bolunen5
+            // 
+            this.bolunen5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bolunen5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bolunen5.ForeColor = System.Drawing.Color.Blue;
+            this.bolunen5.Location = new System.Drawing.Point(295, 208);
+            this.bolunen5.Name = "bolunen5";
+            this.bolunen5.Size = new System.Drawing.Size(200, 44);
+            this.bolunen5.TabIndex = 3;
+            this.bolunen5.Text = "5-ə bölünən";
+            this.bolunen5.UseVisualStyleBackColor = true;
+            // 
+            // ReqemCemi
+            // 
+            this.ReqemCemi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ReqemCemi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReqemCemi.ForeColor = System.Drawing.Color.Blue;
+            this.ReqemCemi.Location = new System.Drawing.Point(548, 208);
+            this.ReqemCemi.Name = "ReqemCemi";
+            this.ReqemCemi.Size = new System.Drawing.Size(200, 44);
+            this.ReqemCemi.TabIndex = 4;
+            this.ReqemCemi.Text = "Ədədlərin cəmi";
+            this.ReqemCemi.UseVisualStyleBackColor = true;
+            // 
+            // ededtersi
+            // 
+            this.ededtersi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ededtersi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ededtersi.ForeColor = System.Drawing.Color.Blue;
+            this.ededtersi.Location = new System.Drawing.Point(12, 309);
+            this.ededtersi.Name = "ededtersi";
+            this.ededtersi.Size = new System.Drawing.Size(200, 44);
+            this.ededtersi.TabIndex = 5;
+            this.ededtersi.Text = "Tərsi";
+            this.ededtersi.UseVisualStyleBackColor = true;
+            this.ededtersi.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // sade
+            // 
+            this.sade.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sade.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sade.ForeColor = System.Drawing.Color.Blue;
+            this.sade.Location = new System.Drawing.Point(295, 309);
+            this.sade.Name = "sade";
+            this.sade.Size = new System.Drawing.Size(200, 44);
+            this.sade.TabIndex = 6;
+            this.sade.Text = "Sadəlik";
+            this.sade.UseVisualStyleBackColor = true;
+            // 
+            // faktorial
+            // 
+            this.faktorial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.faktorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.faktorial.ForeColor = System.Drawing.Color.Blue;
+            this.faktorial.Location = new System.Drawing.Point(548, 309);
+            this.faktorial.Name = "faktorial";
+            this.faktorial.Size = new System.Drawing.Size(200, 44);
+            this.faktorial.TabIndex = 7;
+            this.faktorial.Text = "Faktorial";
+            this.faktorial.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.faktorial);
+            this.Controls.Add(this.sade);
+            this.Controls.Add(this.ededtersi);
+            this.Controls.Add(this.ReqemCemi);
+            this.Controls.Add(this.bolunen5);
+            this.Controls.Add(this.kvadratkub);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Ədədlərin Analizi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button kvadratkub;
+        private System.Windows.Forms.Button bolunen5;
+        private System.Windows.Forms.Button ReqemCemi;
+        private System.Windows.Forms.Button ededtersi;
+        private System.Windows.Forms.Button sade;
+        private System.Windows.Forms.Button faktorial;
+    }
+}
+
